2e0e2fb89f40b519a0b91493a59dfa2c559dfb71
Ahmed Ragheb <AhmedRagheb1712@outlook.com>
2026-03-22 00:25:54 +0200

    Add Password and Code Request and Validator

---
+++ Template.Api/Contracts/Auth/ResetPasswordRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Template.Api.Abstractions.Consts;
+
+namespace Template.Api.Contracts.Auth;
+public class ResetPasswordRequestValidator : AbstractValidator<ResetPasswordRequest>
+{
+	public ResetPasswordRequestValidator()
+	{
+		RuleFor(x => x.Email)
+			.NotEmpty()
+			.EmailAddress();
+
+		RuleFor(x => x.Code)
+			.NotEmpty()
+			.MaximumLength(6);
+
+		RuleFor(x => x.NewPassword)
+				.NotEmpty()
+				.Matches(RegexPatterns.Password)
+				.WithMessage("Password should be at least 8 digits and should contains Lowercase, Uppercase and NonAlphanumeric");
+
+		RuleFor(x => x.ConfirmPassword)
+			.NotEmpty()
+			.Equal(x => x.NewPassword);
+	}
+}
---
+++ Template.Api/Contracts/Auth/VerifyResetCodeRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Template.Api.Contracts.Auth;
+
+public class VerifyResetCodeRequestValidator : AbstractValidator<VerifyResetCodeRequest>
+{
+	public VerifyResetCodeRequestValidator()
+	{
+		RuleFor(x => x.Email)
+			.NotEmpty()
+			.EmailAddress();
+
+		RuleFor(x => x.Code)
+			.NotEmpty()
+			.MaximumLength(6);
+	}
+}
+
