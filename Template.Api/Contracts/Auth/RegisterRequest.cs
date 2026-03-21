7998b75cb95bc51e47d94d97514c1876b7a269b6
Ahmed Ragheb <AhmedRagheb1712@outlook.com>
2026-03-21 15:09:36 +0200

    Add Register Request Validator

--- Template.Api/Contracts/Auth/RegisterRequest.cs
+++ Template.Api/Contracts/Auth/RegisterRequest.cs
@@ -1,15 +1,12 @@
 ﻿namespace Template.Api.Contracts.Auth;
 
-public record AuthResponse(
-	string Id,
-	string? Email,
+public record RegisterRequest(
+	string Email,
 	string FirstName,
 	string LastName,
 	string PhoneNumber,
 	DateOnly DateOfBirth,
 	string Gender,
-	string Token,
-	int ExpiresIn,
-	string RefreshToken,
-	DateTime RefreshTokenExpiration
+	string Password,
+	string ConfirmPassword
 );
---
+++ Template.Api/Contracts/Auth/RegisterRequestValidator.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using Template.Api.Abstractions.Consts;
+
+namespace Template.Api.Contracts.Auth;
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+	public RegisterRequestValidator()
+	{
+		RuleFor(x => x.Email)
+					.NotEmpty()
+					.EmailAddress();
+
+		RuleFor(x => x.FirstName)
+					.NotEmpty()
+					.Length(3, 250)
+					.Matches(RegexPatterns.CharactersOnly_Eng)
+					.WithMessage("Only English letters are allowed.");
+
+		RuleFor(x => x.LastName)
+					.NotEmpty()
+					.Length(3, 250)
+					.Matches(RegexPatterns.CharactersOnly_Eng)
+					.WithMessage("Only English letters are allowed.");
+
+		RuleFor(x => x.Gender)
+					.IsInEnum()
+					.WithMessage("Invalid gender value.");
+
+		RuleFor(x => x.PhoneNumber)
+					.NotEmpty()
+					.Matches(RegexPatterns.PhoneNumber)
+					.WithMessage("Invalid mobile number.");
+
+		RuleFor(x => x.DateOfBirth)
+					.NotEmpty()
+					.LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today));
+
+		RuleFor(x => x.Password)
+					.NotEmpty()
+					.Matches(RegexPatterns.Password)
+					.WithMessage("Password should be at least 8 digits and should contains Lowercase, Uppercase and NonAlphanumeric");
+
+		RuleFor(x => x.ConfirmPassword)
+					.NotEmpty()
+					.Equal(x => x.Password)
+					.WithMessage("Confirm password must be equal password.");
+	}
+}
