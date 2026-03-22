82bee1486408e654934e49c955e9e034371a5a98
Ahmed Ragheb <AhmedRagheb1712@outlook.com>
2026-03-23 01:39:37 +0200

    Add Google Auth Request and Validator and Congifguarions

---
+++ Template.Api/Settings/GoogleSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Template.Api.Settings;
+public class GoogleSettings
+{
+	[Required]
+	public string ClientId { get; set; } = string.Empty;
+
+	[Required]
+	public string ClientSecret { get; set; } = string.Empty;
+}
