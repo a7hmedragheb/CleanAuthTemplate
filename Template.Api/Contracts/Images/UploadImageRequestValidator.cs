9bc435946393bee130cc2e6d1890795a2225704a
Ahmed Ragheb <AhmedRagheb1712@outlook.com>
2026-03-27 22:11:14 +0200

    Add Upload Image Request and Set Global Validator on File Size

---
+++ Template.Api/Contracts/Images/UploadImageRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Template.Api.Contracts.Images.common;
+
+namespace Template.Api.Contracts.Images;
+public class UploadImageRequestValidator : AbstractValidator<UploadImageRequest>
+{
+	public UploadImageRequestValidator()
+	{
+		RuleFor(x => x.Image)
+		 .NotNull()
+		 .WithMessage("Avatar is required.")
+		 .SetValidator(new FileSizeValidator());
+	}
+}
---
+++ Template.Api/Settings/FileSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Template.Api.Settings;
+
+public static class FileSettings
+{
+
+	public const int MaxFileSizeInMB = 5;
+	public const long MaxFileSizeInBytes = MaxFileSizeInMB * 1024 * 1024;
+
+}
