17e1fc680a9aea2da003a7e3e4af6b61157771b8
Ahmed Ragheb <AhmedRagheb1712@outlook.com>
2026-04-29 15:36:11 +0300

    Add Hangfire Extension

---
+++ Template.Api/Extensions/HangfireExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Template.Api.Extensions;
+
+public static class HangfireExtensions
+{
+	public static void UseRecurringJobs(this WebApplication app)
+	{
+		using var scope = app.Services.CreateScope();
+		var recurringJobManager = scope.ServiceProvider
+			.GetRequiredService<IRecurringJobManager>();
+
+		recurringJobManager.AddOrUpdate<CleanUpExpiredRefreshTokensJob>(
+			"cleanup-expired-refresh-tokens",
+			job => job.ExecuteAsync(),
+			Cron.Daily()
+		);
+
+		recurringJobManager.AddOrUpdate<CleanUpExpiredPasswordResetCodesJob>(
+			"cleanup-expired-reset-codes",
+			job => job.ExecuteAsync(),
+			Cron.Daily()
+		);
+	}
+}
